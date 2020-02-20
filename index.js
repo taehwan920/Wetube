226e390ee0d6ea02d3a40d7744cbcb9f0a36db28
Yang Taehwan <taehwan920@gmail.com>
2020-02-20 14:52:30 +0900

    many middleware libraries

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from "express"; // == const express = require('express');
+import morganLogger from 'morgan';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
 const app = express();
 
 const PORT = 4000;
@@ -15,7 +19,12 @@ const betweenHome = (req, res, next) => {
     next();
 };
 
-app.use(betweenHome);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morganLogger('dev'));
+app.use(helmet());
+
 
 app.get("/", handleHome);
 
