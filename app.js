bcbdbd148281f7e4be99653fcdc2f211c9973b25
Yang Taehwan <taehwan920@gmail.com>
2020-02-24 13:47:41 +0900

    made middleware that could apply local data by global && res.render is important in controller

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-import express from "express"; // == const express = require('express');
-import morganLogger, { format } from 'morgan';
-import helmet from 'helmet';
-import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import express from "express"; // == const express = require('express');
+import helmet from 'helmet';
+import { localsMiddleware } from "./middleware";
+import morganLogger, { format } from 'morgan';
+import globalRouter from './routers/globalRouter';
 import userRouter from './routers/userRouter';
 import videoRouter from './routers/videoRouter';
-import globalRouter from './routers/globalRouter';
 import routes from './routes'
+
 const app = express();
 
+app.use(helmet());
 app.set('view engine', "pug")
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morganLogger('dev'));
-app.use(helmet());
+
+app.use(localsMiddleware)
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,7 @@
 export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.send("search");
-export const videos = (req, res) => res.send("videos");
-export const upload = (req, res) => res.send("upload");
-export const videoDetail = (req, res) => res.send("video Detail");
-export const editVideo = (req, res) => res.send("edit Video");
-export const deleteVideo = (req, res) => res.send("delete Video");
+export const search = (req, res) => res.render("search");
+export const videos = (req, res) => res.render("videos");
+export const upload = (req, res) => res.render("upload");
+export const videoDetail = (req, res) => res.render("video Detail");
+export const editVideo = (req, res) => res.render("editVideo");
+export const deleteVideo = (req, res) => res.render("deleteVideo");
