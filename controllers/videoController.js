4752e5f24abc06d8736c6c635a656bc80e6cdd4f
Yang Taehwan <taehwan920@gmail.com>
2020-02-24 12:30:18 +0900

    construct layout and apply it to views

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,7 @@
 export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.send("Search");
-export const videos = (req, res) => res.send("Videos");
-export const upload = (req, res) => res.send("Upload");
-export const videoDetail = (req, res) => res.send("Video Detail");
-export const editVideo = (req, res) => res.send("Edit Video");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const search = (req, res) => res.send("search");
+export const videos = (req, res) => res.send("videos");
+export const upload = (req, res) => res.send("upload");
+export const videoDetail = (req, res) => res.send("video Detail");
+export const editVideo = (req, res) => res.send("edit Video");
+export const deleteVideo = (req, res) => res.send("delete Video");
