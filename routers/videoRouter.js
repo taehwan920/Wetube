de7c0a939e0576da9e5e5dbea80fc3f878c52b6b
Yang Taehwan <taehwan920@gmail.com>
2020-03-05 22:45:53 +0900

    upload Video with multar middleware!

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
 import routes from "../routes";
 import { editVideo, deleteVideo, getUpload, postUpload, videoDetail } from "../controllers/videoController";
+import { uploadVideo } from "../middleware";
 
 const videoRouter = express.Router();
 
 videoRouter.get(routes.upload, getUpload);
-videoRouter.post(routes.upload, postUpload);
+videoRouter.post(routes.upload, uploadVideo, postUpload);
 
 videoRouter.get(routes.videoDetail(), videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
