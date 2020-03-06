d8eda12db0a0ceb18c8b542c6383414e0a0d852b
Yang Taehwan <taehwan920@gmail.com>
2020-03-07 02:15:02 +0900

    how to get Video by ID & modified videoDetail page in pug & modified videoDetail controller in videoController, so we could get video by try/catch and ID

--- routes.js
+++ routes.js
@@ -40,7 +40,7 @@ const routes = {
     upload: UPLOAD,
     videoDetail: id => {
         if (id) {
-            return id
+            return `/videos/${id}`
         } else {
             return VIDEO_DETAIL;
         }
