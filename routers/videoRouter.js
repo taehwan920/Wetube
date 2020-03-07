390df08d20393c8f421ddf9170d333e3130f235f
Yang Taehwan <taehwan920@gmail.com>
2020-03-08 01:10:58 +0900

    modified editVideo to getEditVideo and added postEditVideo. also modified editVideo and videoDetail pug file.

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,15 +1,22 @@
 import express from "express";
 import routes from "../routes";
-import { editVideo, deleteVideo, getUpload, postUpload, videoDetail } from "../controllers/videoController";
+import { deleteVideo, getUpload, postUpload, videoDetail, postEditVideo, getEditVideo } from "../controllers/videoController";
 import { uploadVideo } from "../middleware";
 
 const videoRouter = express.Router();
 
+// Upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
 
+// VideoDetail
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
+
+// EditVideo
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+// deleteVideo
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
