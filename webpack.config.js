05716cee8210a0ddd803bc4c2ab4f149b9a5f188
Yang Taehwan <taehwan920@gmail.com>
2020-03-10 01:13:58 +0900

    added new rule in webpack, modified main.pug in layouts and added app.use for static directory in app.js

--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,16 @@ const OUTPUT_DIR = path.join(__dirname, "static");
 const config = {
     entry: ENTRY_FILE,     // entry와 output을 지정.
     mode: MODE,
-    module: {               //***module 파트의 의미 :  // scss파일을 전부 찾고, 그걸 css로 바꾸고, 전체 텍스트 중 그 css의 텍스트를 추출하고, 추출된 css를 분리된 파일로 만드는 작업.
+    module: {               //***module 파트의 의미 :  // 이번엔 scss파일을 전부 찾고, 그걸 css로 바꾸고, 전체 텍스트 중 그 css의 텍스트를 추출하고, 추출된 css를 분리된 파일로 만드는 작업. -> 다른 파일 포맷을 위한 특정 변환 룰.
         rules: [                //*** 다음과 같은 rules를 따를것.
+            {
+                test: /\.(js)$/,
+                use: [
+                    {
+                        loader: "babel-loader",
+                    }
+                ]
+            },
             {
                 test: /\.(scss)$/,  //*** 어떤 조건을 알아보라(test)는 것. 즉 .scss파일을 만나게 되면 다음과 같은 plugin을 사용하도록.(use부분)
                 use: ExtractCSS.extract([                   //***이 plugin은 내부에서 또 plugin을 사용중. SCSS를 일반 CSS로 통역해야해서.
