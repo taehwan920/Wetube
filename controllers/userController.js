3f65a09bacdbdd37d2fa2bda15d506ad94aec161
Yang Taehwan <taehwan920@gmail.com>
2020-03-23 22:00:01 +0900

    brought githubLogin strategy from passport JS and modified logout controller function

--- controllers/userController.js
+++ controllers/userController.js
@@ -32,12 +32,18 @@ export const getLogin = (req, res) => res.render("login", { pageTitle: "Login" }
 export const postLogin = passport.authenticate('local', {
     failureRedirect: routes.login,
     successRedirect: routes.home
-})
+});
+
+export const githubLogin = ;
+
+export const githubLoginCallback = (accessToken, refreshToken, profile, cb) => {
+    console.log(accessToken, refreshToken, profile, cb);
+};
 
 export const logout = (req, res) => {
-    //To Do: Process Log Out
+    req.logout();
     res.redirect(routes.home);
-}
+};
 
 export const users = (req, res) => res.render("users", { pageTitle: "Users" });
 export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Detail" });
--- passport.js
+++ passport.js
@@ -1,7 +1,17 @@
+import GithubStrategy from "passport-github";
 import passport from "passport";
 import User from "./models/User";
+import { githubLoginCallback } from "./controllers/userController";
 
 passport.use(User.createStrategy());
+
+passport.use(new GithubStrategy({
+    clientID: process.env.GH_ID,
+    clientSecret: process.env.GH_SECRET,
+    callbackURL: "http://localhost:4000/auth/github/callback"
+}),
+    githubLoginCallback
+);
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
