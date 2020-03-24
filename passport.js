2cc447ab4c7a3a692f688a619e9d563a4cb6f04c
Yang Taehwan <taehwan920@gmail.com>
2020-03-24 16:16:48 +0900

    now new users could join with github but this is not perfect.

--- passport.js
+++ passport.js
@@ -1,16 +1,19 @@
 import GithubStrategy from "passport-github";
 import passport from "passport";
+import routes from "./routes"
 import User from "./models/User";
 import { githubLoginCallback } from "./controllers/userController";
 
 passport.use(User.createStrategy());
 
-passport.use(new GithubStrategy({
-    clientID: process.env.GH_ID,
-    clientSecret: process.env.GH_SECRET,
-    callbackURL: "http://localhost:4000/auth/github/callback"
-}),
+passport.use(new GithubStrategy(
+    {
+        clientID: process.env.GH_ID,
+        clientSecret: process.env.GH_SECRET,
+        callbackURL: `http://localhost:4000${routes.githubCallback}`
+    },
     githubLoginCallback
+)
 );
 
 passport.serializeUser(User.serializeUser());
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
+import passport from "passport";
 import routes from "../routes";
 import { home, search } from "../controllers/videoController";
-import { getLogin, postLogin, logout, getJoin, postJoin } from "../controllers/userController";
-import { onlyPublic } from "../middleware";
+import { getLogin, postLogin, logout, getJoin, postJoin, githubLogin, postGithubLogin } from "../controllers/userController";
+import { onlyPublic, onlyPrivate } from "../middleware";
 
 const globalRouter = express.Router();
 
@@ -13,7 +14,15 @@ globalRouter.get(routes.login, onlyPublic, getLogin);
 globalRouter.post(routes.login, onlyPublic, postLogin);
 
 globalRouter.get(routes.home, home);
-globalRouter.get(routes.logout, onlyPublic, logout);
+globalRouter.get(routes.logout, onlyPrivate, logout);
 globalRouter.get(routes.search, search);
+
+globalRouter.get(routes.github, githubLogin);
+
+globalRouter.get(
+    routes.githubCallback,
+    passport.authenticate('github', { failureRedirect: '/login' }),
+    postGithubLogin
+);
 
 export default globalRouter;
